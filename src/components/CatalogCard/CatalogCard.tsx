bd37fab22d9b44e10fe10eb558bac26ad9ccc786
vean <vean@veans-mbp-2.home>
2023-10-11 12:28:56 +0200

    feature: add short description in catalog

--- src/components/CatalogCard/CatalogCard.tsx
+++ src/components/CatalogCard/CatalogCard.tsx
@@ -8,6 +8,7 @@ import {
 import { useNavigate } from 'react-router';
 import './CatalogCard.scss';
 import BasicRating from '../UserRating';
+import { useMemo } from 'react';
 
 interface ICatalogCardProps {
   firstName: string;
@@ -16,6 +17,7 @@ interface ICatalogCardProps {
   avatar: string;
   city: string;
   country: string;
+  about: string;
 }
 
 const CatalogCard = ({
@@ -25,8 +27,18 @@ const CatalogCard = ({
   avatar,
   city,
   country,
+  about,
 }: ICatalogCardProps) => {
   const navigate = useNavigate();
+  const textLengthLimit = 100;
+
+  const trimText = useMemo(
+    (): string =>
+      about.length <= textLengthLimit
+        ? about
+        : about.slice(0, textLengthLimit) + '...',
+    [about]
+  );
 
   return (
     <Grid item xs={12} sm={6} md={4} data-testid="catalog-card-test">
@@ -47,6 +59,9 @@ const CatalogCard = ({
             <BasicRating readOnly />
             <Typography variant="h3">
               {firstName} {lastName}
+            </Typography>
+            <Typography variant="h4" textAlign="justify" padding={3}>
+              {trimText}
             </Typography>
             <Typography variant="h5">
               {country ? `${country},` : ''} {city}
