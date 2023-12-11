b6bb71ae84bc7cf63b5259fa2fffee3826c9b4c3
vean <vean@veans-mbp-2.home>
2023-12-11 13:17:00 +0100

    refactoring

---
+++ src/components/PortfolioPost/PortfolioPost.tsx
@@ -0,0 +1,47 @@
+import { IProfilePortfolio } from '@interfaces/index';
+import { Container, Grid, Typography } from '@mui/material';
+import { ReactElement, useMemo } from 'react';
+import AliceCarousel from 'react-alice-carousel';
+
+const PortfolioPost = ({ data }: { data?: IProfilePortfolio }) => {
+  const SliderItems = useMemo((): ReactElement<string, string>[] => {
+    if (data) {
+      return data.photo_post.map((el) => (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div
+            style={{
+              backgroundImage: `url(${el.photo})`,
+              backgroundPosition: 'center',
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
+              height: '30rem',
+              width: '30rem',
+            }}
+          />
+        </div>
+      ));
+    }
+    return [];
+  }, [data]);
+
+  return (
+    <Container maxWidth="lg">
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <AliceCarousel mouseTracking items={SliderItems} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h4" marginBottom={5} color="pink">
+            {' '}
+            {data?.work_type?.name}
+          </Typography>
+          <Typography variant="h5" textAlign="justify">
+            {data?.description}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default PortfolioPost;
--- src/pages/PortfolioPostPage/PortfolioPostPage.tsx
+++ src/pages/PortfolioPostPage/PortfolioPostPage.tsx
@@ -1,50 +1,12 @@
-import { Container, Grid, Typography } from '@mui/material';
+import PortfolioPost from '@components/PortfolioPost/PortfolioPost';
 import { useGetPortfolioPostQuery } from '@services/profileApi';
-import { ReactElement, useMemo } from 'react';
-import AliceCarousel from 'react-alice-carousel';
 import { useParams } from 'react-router';
 
 const PortfolioPostPage = () => {
   const { id } = useParams();
   const { data } = useGetPortfolioPostQuery(id || '');
-  const SliderItems = useMemo((): ReactElement<string, string>[] => {
-    if (data) {
-      return data.photo_post.map((el) => (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <div
-            style={{
-              backgroundImage: `url(${el.photo})`,
-              backgroundPosition: 'center',
-              backgroundSize: 'contain',
-              backgroundRepeat: 'no-repeat',
-              height: '30rem',
-              width: '30rem',
-            }}
-          ></div>
-        </div>
-      ));
-    }
-    return [];
-  }, [data]);
 
-  return (
-    <Container maxWidth="lg">
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <AliceCarousel mouseTracking items={SliderItems} />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h4" marginBottom={5} color="pink">
-            {' '}
-            {data?.work_type?.name}
-          </Typography>
-          <Typography variant="h5" textAlign="justify">
-            {data?.description}
-          </Typography>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+  return <PortfolioPost data={data} />;
 };
 
 export default PortfolioPostPage;
