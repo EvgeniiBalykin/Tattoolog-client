ad1c3dfc5f90442956663933d4f679dd942b82f9
vean <vean@veans-mbp-1.home>
2024-01-03 15:46:04 +0100

    fix: fix meta blog

--- src/pages/FestivalsPage/FestivalsPage.tsx
+++ src/pages/FestivalsPage/FestivalsPage.tsx
@@ -2,14 +2,16 @@ import FestivalPosts from '@components/FestivalPosts/FestivalPosts';
 import { MainImageBox } from '@components/Home';
 import { FestivalImg } from '@images/index';
 import { Container } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 const FestivalsPage = () => {
+  const { t } = useTranslation();
   return (
     <>
       <MainImageBox
         img={FestivalImg}
-        title="Exploring the Art of Ink: A Journey into the World of Tattoos"
-        subtitle="."
+        title={t('pages.festival_page.title')}
+        subtitle={t('pages.festival_page.subtitle')}
       />
       <Container maxWidth="lg">
         <FestivalPosts />
