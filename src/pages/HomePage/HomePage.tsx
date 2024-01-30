f170530e3f3bdc99939834f2c20c1ba2a336189d
vean <vean@veans-mbp-3.home>
2024-01-30 14:10:24 +0100

    fix: small fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,12 +3,22 @@ import {
   WhyUsMain,
   JoinUsMain,
   PartnersSlider,
+  MainImageBox,
 } from '@components/Home';
+import { useTranslation } from 'react-i18next';
+import { HOME_IMAGE_BOX } from './contants';
 import './HomePage.scss';
 
 export const HomePage = () => {
+  const { t } = useTranslation();
   return (
     <>
+      <MainImageBox
+        title={t(HOME_IMAGE_BOX.title)}
+        subtitle={t(HOME_IMAGE_BOX.subtitle)}
+        buttons={HOME_IMAGE_BOX.buttons}
+        img={HOME_IMAGE_BOX.img}
+      />
       <AboutMain />
       <WhyUsMain />
       <JoinUsMain />
