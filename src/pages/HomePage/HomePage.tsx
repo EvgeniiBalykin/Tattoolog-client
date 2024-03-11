cb0e75a1b1a238e00c12020a68fc59cfd0c8028f
vean <vean@device-10020.home>
2024-03-11 10:28:56 +0100

    fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,39 +5,15 @@ import {
   PartnersSlider,
   MainImageBox,
 } from '@components/Home';
-import { MainHandImg } from '@images/index';
-import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import { HOME_IMAGE_BOX } from './contants';
 import './HomePage.scss';
 
 export const HomePage = () => {
   const { t } = useTranslation();
-  const currentUrl = window.location.href;
 
   return (
     <>
-      <Helmet>
-        <title>Tattoolog</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="description"
-          content="Global catalog of tattoo artists and studios"
-        />
-        <meta
-          name="keywords"
-          content="tattoolog tattoo catalog artist studio worldwide"
-        />
-        <meta property="og:title" content="Tattoolog" />
-        <meta property="og:description" content="Discover tattoo world now!" />
-        <meta property="og:type" content="main" />
-        <meta property="og:url" content={currentUrl} />
-        <meta
-          property="og:image"
-          content={`https://tattoolog.pl${MainHandImg}`}
-        />
-      </Helmet>
       <MainImageBox
         title={t(HOME_IMAGE_BOX.title)}
         subtitle={t(HOME_IMAGE_BOX.subtitle)}
